d7ddc3bd0f714c06c80853a883b44a76b4b2ae3b
Arvid Jakobsson <arvid.jakobsson@ahrefs.com>
2025-11-10 10:41:31 +0100

    fixup! feat: annotation function for [justl-exec-recipe-in-dir]

--- justl.el
+++ justl.el
@@ -504,12 +504,12 @@ They are returned as objects, as per the JSON output of \"just --dump\"."
   (when arg
     (cadr (s-split "--justfile=" arg))))
 
-(defvar justl-recipes nil
+(defvar justl--recipes nil
   "Set of recipes loaded for `justl-exec-recipe-in-dir'")
 
 (defun justl-completion-annotation (candidate)
   "Annotation function for `justl-exec-recipe-in-dir'."
-  (let* ((recipes justl-recipes)
+  (let* ((recipes justl--recipes)
          (doc (cl-some
                (lambda (recipe)
                  (when (string= (justl--recipe-name recipe) candidate)
@@ -525,9 +525,9 @@ They are returned as objects, as per the JSON output of \"just --dump\"."
   (let* ((justfile (justl--find-justfile default-directory)))
     (unless justfile
       (error "No justfile found"))
-    (setq justl-recipes (justl--get-recipes justfile))
-    (let* ((recipes justl-recipes)
-           (recipe-names (mapcar 'justl--recipe-name justl-recipes))
+    (setq justl--recipes (justl--get-recipes justfile))
+    (let* ((recipes justl--recipes)
+           (recipe-names (mapcar 'justl--recipe-name justl--recipes))
            (recipe-name (completing-read "Recipe: "
                                          (lambda (string pred action)
                                            (if (eq action 'metadata)
